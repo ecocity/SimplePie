f7320a83b9d6f87869f5b5824306ce01b12bfd12
Geoffrey Sneddon <geoffrey@simplepie.org>
2007-08-10 17:01:23 +0000

    And I better add the schema

---
+++ db.sql
@@ -0,0 +1,34 @@
+/* SQLite */
+CREATE TABLE cache_data (
+	id TEXT NOT NULL,
+	is_feed TINYINT NOT NULL,
+	data BLOB NOT NULL,
+	mtime INTEGER UNSIGNED NOT NULL
+);
+CREATE UNIQUE INDEX id ON cache_data(id);
+
+CREATE TABLE items (
+	feed_id TEXT NOT NULL,
+	id TEXT NOT NULL,
+	data TEXT NOT NULL,
+	posted INTEGER UNSIGNED NOT NULL
+);
+
+
+/* MySQL */
+CREATE TABLE `cache_data` (
+	`id` TEXT CHARACTER SET utf8 NOT NULL,
+	`is_feed` TINYINT NOT NULL,
+	`data` BLOB NOT NULL,
+	`mtime` INT UNSIGNED NOT NULL,
+	UNIQUE (
+		`id`(125)
+	)
+);
+
+CREATE TABLE `items` (
+	`feed_id` TEXT CHARACTER SET utf8 NOT NULL,
+	`id` TEXT CHARACTER SET utf8 NOT NULL,
+	`data` TEXT CHARACTER SET utf8 NOT NULL,
+	`posted` INT UNSIGNED NOT NULL
+);
